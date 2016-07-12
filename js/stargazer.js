6b90bd1e0d7699f68edfe99c173f6931b8ec7067
Susannah Waite <susannahwaite2@gmail.com>
2016-07-12 08:28:15 -0500

    stargazer homework complete

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,26 @@
+$(document).ready(function(){
+  $('#submit').click(function(event){
+    event.preventDefault();
+    var name = $('#search-name').val();
+    search(name);
+
+
+  });
+
+});
+
 function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+    var api = 'http://www.strudel.org.uk/lookUP/json/?name='+ name;
+    // requires "jsonp" datatype.
+
+      $.ajax({
+        method: "GET",
+        url: api,
+        dataType: "jsonp",
+        jsonpCallback: "showImage"
+      })
 }
+function showImage(json) {
+  var image = json.image.src;
+  $('#preview').html(`<div><img src="${image}"></div>`);
+  }
